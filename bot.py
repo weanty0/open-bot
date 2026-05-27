96ca158d6b5813204327dac70fd1b67ff62079c8
Weanty <rajcziattila.2@gmail.com>
2026-05-27 16:47:27 +0200

    Initial

---
+++ bot.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+from datetime import datetime
+
+intents = discord.Intents.default();
+intents.message_content = True;
+
+bot = commands.Bot(command_prefix='?', intents=intents);
+
+# Log
+
+def log_use(ctx, *args) -> int:
+    x = datetime.now()
+    print(f"Log_command: {ctx.author} used command: {ctx.command}. Arguments: {args}. Log generated: {x.strftime("%d/%m/%Y %H:%M:%S")}.");
+    return 1;
+
+# Commands
+
+@bot.command()
+async def hey(ctx, *args) -> None:
+    log_use(ctx, args);
+
+# Misc
+
+def readToken(path : str) -> str:
+    with open(path, "r") as f:
+        ctx = f.read();
+        return ctx;
+
+# Run
+
+bot.run(readToken("token.txt"));
