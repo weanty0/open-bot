fc265842f9dc55c59899ac5ce41e1406140ad4c4
Weanty <rajcziattila.2@gmail.com>
2026-05-28 13:25:24 +0200

    Added basic hey command + logging for using commands.

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ bot = commands.Bot(command_prefix='?', intents=intents);
 
 # Log
 
-def log_use(ctx, *args) -> int:
+async def log_use(ctx, *args) -> int:
     x = datetime.now()
     print(f"Log_command: {ctx.author} used command: {ctx.command}. Arguments: {args}. Log generated: {x.strftime("%d/%m/%Y %H:%M:%S")}.");
     return 1;
@@ -18,7 +18,8 @@ def log_use(ctx, *args) -> int:
 
 @bot.command()
 async def hey(ctx, *args) -> None:
-    log_use(ctx, args);
+    if await log_use(ctx, args):
+        await ctx.send(f"Hey {ctx.author.mention}! How are ya?", mention_author=True);
 
 # Misc
 
