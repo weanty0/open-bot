65ce5db3d00cad4b4c96e835fb4e2aadd1c3d1f2
Weanty <rajcziattila.2@gmail.com>
2026-06-02 14:13:41 +0200

    Added verbose mode to the bot. You can turn it on with '?eval verbose true'

--- bot.py
+++ bot.py
@@ -22,6 +22,10 @@ err = {
     100 : "Couldn't evaluate the given command."
 };
 
+dat = {
+    "verbose" : False
+}
+
 class Error:
     def __init__(self, ctx : dict,level : int, e : int):
         self.level = level;
@@ -30,14 +34,21 @@ class Error:
     async def log_error(self) -> int:
         x = datetime.now();
         y = x.strftime("%d/%m/%Y %H:%M:%S");
-        print(f"Error_{self.e}: {self.ctx.author} at {y} used {self.ctx.command}. ec_{self.level}: {err[self.level]}.");
+        ret = f"Error_{self.e}: {self.ctx.author} at {y} used {self.ctx.command}. ec_{self.level}: {err[self.level]}.";
+        print(ret);
+        if dat["verbose"]:
+            await self.ctx.send(f"```{ret}```");
+        return (0, ret);
 
 # Log
 
-async def log_use(ctx, *args) -> int:
+async def log_use(ctx, *args) -> tuple:
     x = datetime.now()
-    print(f"Log_command: {ctx.author} used command: {ctx.command}. Arguments: {args}. Log generated: {x.strftime("%d/%m/%Y %H:%M:%S")}.");
-    return 1;
+    ret = f"Log_command: {ctx.author} used command: {ctx.command}. Arguments: {args}. Log generated: {x.strftime("%d/%m/%Y %H:%M:%S")}.";
+    print(ret);
+    if dat["verbose"]:
+        await ctx.send(f"```{ret}```");
+    return (0, ret);
 
 async def throw_error(ctx : dict = {}, args : tuple = (), errorLevel : int = 99) -> int:
     tmp = Error(ctx, errorLevel, err[0]);
@@ -46,9 +57,13 @@ async def throw_error(ctx : dict = {}, args : tuple = (), errorLevel : int = 99)
     return 0;
 
 async def toggle_verbose(ctx : dict, args : tuple) -> int :
-    print(f"-v : {verb}.");
-    print(f"{args}, len {len(args)}");
-    return 0;
+    print(f"-v: {dat["verbose"]}");
+    if len(args) > 1:
+        if args[1] == 'true': 
+            dat["verbose"] = True;
+        elif args[1] == 'false': 
+            dat["verbose"] = False;
+    return (0);
 
 
 # Evals
@@ -73,7 +88,10 @@ async def hey(ctx, *args) -> None:
 async def eval(ctx, *args) -> None:
     if await log_use(ctx, args):
         if ctx.author.id == bot_owner:
-            print(f"{args[0]}");
+            if args[0] in list(eva.keys()):
+                rv = await eva[args[0]](ctx, args);
+                if rv == ():
+                    await throw_error(ctx, args, 4);
         else:
             await throw_error(ctx, args, 3);
     else:
